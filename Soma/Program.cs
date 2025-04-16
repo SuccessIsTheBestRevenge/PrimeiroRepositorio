2d51181da0a72b93c94f4cbf02473d4f20d5fa9a
Fernando Crespo <crespofernandolima@gmail.com>
2025-04-16 18:51:26 -0300

    Primeira Orintaçao a Objetos

---
+++ Soma/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+
+    public class Calculadora
+{
+    public double Numero1 { get; set; }
+    public double Numero2 { get; set; }
+
+    public Calculadora(double numero1, double numero2)
+    {
+        Numero1 = numero1;
+        Numero2 = numero2;
+    }
+    public double Somar()
+    {
+        return Numero1 + Numero2;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Digite o Primeiro Número:");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Digite o Segundo Número:");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Calculadora calc = new Calculadora(num1, num2);
+
+        Console.WriteLine($"\nResultado da soma: {calc.Somar()}");
+
+    }
+}
